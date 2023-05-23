da167f7ba69a677eee5b6645febe7aacfe1a5de4
Tsion <tsionwendmu12@gmail.com>
2023-05-22 20:14:15 -0400

    changed some things

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const headingTwo = document.querySelector('#word')
 const paragraphThree = document.querySelector('#synonym')
 const audio = document.querySelector('#audioButton')
 const audioPlayer = new Audio();
+const soundIcon = document.querySelector('#speaker-icon')
 
 
 
@@ -18,30 +19,42 @@ document.addEventListener('DOMContentLoaded', () => {
     button.addEventListener('click', async () => {
         let info = textInput.value;
         console.log(info);
-        try {
+        
             let response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${info}`);
-            console.log(response);
+            console.log(response); 
 
-            let word = response.data[0].word;
-            console.log(word)
-            headingTwo.innerHTML = word;
+          let word = response.data[0].word;
+          if (word) {
+           console.log(word)
+           headingTwo.innerHTML = word;
+           audio.style.display = 'inline'
+          }else {
+            console.log('Please enter a correct word')
+            headingTwo.innerHTML = 'Please enter a correct word'
+            audio.style.display = 'none'
+          }
+          
+          
 
-            let cit = response.data[0].meanings[0].definitions[0].definition;
-            console.log(cit);
+           let cit = response.data[0].meanings[0].definitions[0].definition;
+           console.log(cit);
             paragraph.innerText = cit;
 
-            let example = response.data[0].meanings[0].definitions[1].example;
+            let example = response.data[0].meanings[1].definitions[0].example;
+            if (example){
             console.log(example)
             paragraphTwo.innerText = example
+            paragraphTwo.style.display  = "block";
+            } else {
+                console.log('There is no example for this word.')
+                paragraphTwo.innerText = '"There is no example for this word."'
+                paragraphTwo.style.display = "block"
+            }
 
-            let synonym = response.data[0].meanings[1].synonyms;
-            paragraphThree.innerHTML = synonym
+    });  
 
-        } catch (error) {
-            console.log(error);
-        }
-    });
 });
+
 
 
 audio.addEventListener('click', async () => {
@@ -49,14 +62,13 @@ audio.addEventListener('click', async () => {
 
     const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordTwo}`)
     
-    const audioUrl = response.data[0].phonetics[0].audio
+    const audioUrl = response.data[0].phonetics[1].audio
     console.log(audioUrl)
     audioPlayer.src = audioUrl
     audioPlayer.play()
 
   
 });
-
 
 
 
